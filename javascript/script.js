ecbf6286801f81ca1e0cac416f68194414ca13bc
chaitanyaa023 <chaitanya.egilam@gmail.com>
2025-03-21 18:02:58 +0530

    odinson

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,14 @@
-console.log("Java lang");
+
+function Prime(n){
+  for(let i=2; i<n; i++){
+    if (n%i===0) return false;
+  }
+  return true;
+}
+
+function showPrime(n){
+  for (let i=2; i<n; i++){
+    if (Prime(i)) console.log(i);
+  }
+}
+showPrime(10);
